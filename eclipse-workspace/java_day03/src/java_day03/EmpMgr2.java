8ac2114dd2ec40ccea26067657fb645d89a7c3c4
Oratree <neem693@gmail.com>
2018-03-16 13:30:56 +0900

    3_16 update

--- eclipse-workspace/java_day03/src/java_day03/EmpMgr2.java
+++ eclipse-workspace/java_day03/src/java_day03/EmpMgr2.java
@@ -1,18 +1,23 @@
 package java_day03;
 
-public class EmpMgr {
+public class EmpMgr2 {
 	
 	public static void main(String[] args) {
+	//수정내역 : 연산자(산술 연산자 : + , - , *,/) 반영
 	//CTRL + F11 실행단축키
 		//메모리 (변수)선언문
-		int 급여;
+		//변수 = 수식;
+		int 급여; //
+		int 인상률;
 		급여 = 100;
+		인상률= 15;//배율
 		System.out.println(급여);
 		
-		급여 = 200;
+		급여 = 급여 + 급여*인상률/100;
 		System.out.println("인상된 급여");
 		System.out.println(급여);
 		급여 = 0;
+		//연산자 반영
 	
 	}
 	
