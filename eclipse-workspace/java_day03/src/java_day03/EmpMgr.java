155ad593c6b8f61b5235ba1144ce41787178a23d
neem693 <neem693@gmail.com>
2018-03-16 11:51:11 +0900

    java_day03

---
+++ eclipse-workspace/java_day03/src/java_day03/EmpMgr.java
@@ -0,0 +1,17 @@
+package java_day03;
+
+public class EmpMgr {
+	
+	public static void main(String[] args) {
+	//CTRL + F11 실행단축키
+		//메모리 (변수)선언문
+		int 급여;
+		급여 = 100;
+		System.out.println(급여);
+		
+		급여 = 200;
+	
+	}
+	
+
+}
